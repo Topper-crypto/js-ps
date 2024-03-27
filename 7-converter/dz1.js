1f1ef1dd16f2dddfa3b0971c6644c37544c001f6
korovin80@yandex.ru <korovin80@yandex.ru>
2024-03-27 19:26:14 +0300

    dz-fix

--- 7-converter/dz1.js
+++ 7-converter/dz1.js
@@ -1,18 +1,36 @@
-function converter(amount, type) {
-    let rur;
-    if (type === 'usd') {
-        rur = 90 * amount;
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    let exchangeRate;
+    fromCurrency = fromCurrency.toLowerCase();
+    toCurrency = toCurrency.toLowerCase();
+    
+    switch (fromCurrency + '_' + toCurrency) {
+        case 'rub_usd':
+            exchangeRate = 0.013;
+            break;
+        case 'usd_rub':
+            exchangeRate = 75.0;
+            break;
+        case 'rub_eur':
+            exchangeRate = 0.012;
+            break;
+        case 'eur_rub':
+            exchangeRate = 85.0;
+            break;
+        case 'usd_eur':
+            exchangeRate = 0.92;
+            break;
+        case 'eur_usd':
+            exchangeRate = 1.09;
+            break;
+        default:
+            return 0;
     }
-    else if (type === 'de') {
-        rur = 80 * amount;
-    }
-    else if (type === 'ch') {
-        rur = 70 * amount;
-    }
-    else {
-        console.log('тз хз');
-    }
-    return rur;
+
+  
+    const convertedAmount = amount * exchangeRate;
+    return convertedAmount;
 }
 
-console.log(converter(900, 'па'));
+
+console.log(`Примерно $1000 рублей к долларам вернет $${convertCurrency(1000, 'rub', 'usd')}`);
+
