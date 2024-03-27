8054d8dc3585ba85d406a62d971e0503f8d13fdd
korovin80@yandex.ru <korovin80@yandex.ru>
2024-03-27 19:28:35 +0300

    fix

---
+++ practice/practice2.js
@@ -0,0 +1,7 @@
+let age = 18 + 5;
+age += 2; //age = age + 2
+
+console.log(age);
+
+const vasia = 20;
+console.log(age > vasia);
---
+++ practice/practice4.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 5.6;
+
+console.log(typeof a);
+console.log(typeof b);
+a = 'string';
+console.log(typeof a);
+
+let isAdmin = a > 10;
+console.log (typeof isAdmin);
+
+let c;
+console.log(c);
+c = 5;
+console.log(c);
+
+let d = null;
+console.log(typeof d);
