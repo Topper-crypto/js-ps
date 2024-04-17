1567473c61c16ff8ec22aa8fa5cd1d52732b9d4d
korovin80@yandex.ru <korovin80@yandex.ru>
2024-04-17 21:31:04 +0300

    fix

--- 11-date-array/dz-11.js
+++ 11-date-array/dz-11.js
@@ -1,38 +1,27 @@
-function validateDate(dateStr) {
-    const match = dateStr.match(/^(\d{2})[./](\d{2})[./](\d{2})$/);
-    if (match) {
-        const [, day, month, year] = match.map(Number);
-        if (1 <= month && month <= 12 && 1 <= day && day <= 31) {
-            if ([4, 6, 9, 11].includes(month) && day > 30) {
-                return false;
-            } else if (month === 2) {
-              
-                if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-                    if (day > 29) {
-                        return false;
-                    }
-                } else if (day > 28) {
-                    return false;
-                }
+
+function processDates(dateStrings) {
+    const validDates = [];
+    dateStrings.forEach(dateStr => {
+        const dateParts = dateStr.split(/[./]/);
+        let day, month, year;
+
+        if (dateParts.length === 3) {
+            day = parseInt(dateParts[0]);
+            month = parseInt(dateParts[1]);
+            year = parseInt(dateParts[2]);
+
+           
+            if (day <= 31 && month <= 12 && year >= 0) {
+                if (day < 10) day = '0' + day;
+                if (month < 10) month = '0' + month;
+                validDates.push(`${day}/${month}/${year}`);
             }
-            return true;
         }
-    }
-    return false;
+    });
+    return validDates;
 }
 
-function processDates(dateArray) {
-    const processedDates = [];
-    for (const dateStr of dateArray) {
-        if (validateDate(dateStr)) {
-            
-            const [day, month, year] = dateStr.split(/[./]/).map(Number);
-            processedDates.push(`${day.toString().padStart(2, '0')}.${month.toString().padStart(2, '0')}.${year.toString().padStart(2, '0')}`);
-        }
-    }
-    return processedDates;
-}
 
-const dates = ["10.02.22", "11.12.23", "0.13.22", "41.12", "02/29/24", "02/30/24"];
-const processedDates = processDates(dates);
+const dateStrings = ["10.02.22", "11.12.23", "0.13.22", "41.12"];
+const processedDates = processDates(dateStrings);
 console.log(processedDates);
